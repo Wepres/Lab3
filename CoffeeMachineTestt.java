2cae0c9774b6b0589f254c6e8fc14fb7ccaffe47
Wepres <67938611+Wepres@users.noreply.github.com>
2022-05-21 23:23:40 +0200

    Update CoffeeMachineTestt.java

--- CoffeeMachineTestt.java
+++ CoffeeMachineTestt.java
@@ -1,39 +1,72 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
-public class MaxMinElementsTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class CoffeeMachineTest {
     @Test
-    public void test1(){
-        MaxMinElements y = new MaxMinElements();
-        int[] x = {1,2,3,4,-44,111,7,8,9};
-        assertEquals(-44,y.Min(x));
-        assertEquals(111, y.Max(x));
+    public void test1() {
+        coffe_machine c = new coffe_machine();
+        assertEquals("Please turn the machine on", c.addMoney(30));
     }
     @Test
-    public void test2(){
-        MaxMinElements y = new MaxMinElements();
-        int[] x = {1,2,3,4,0,111,7,8,1100};
-        assertEquals(0,y.Min(x));
-        assertEquals(1100, y.Max(x));
+    public void test2() {
+        coffe_machine c = new coffe_machine();
+        assertEquals("Power on" + "Lights on", c.openMachine());
     }
     @Test
-    public void test3(){
-        MaxMinElements y = new MaxMinElements();
-        int[] x = {8,9};
-        assertEquals(8,y.Min(x));
-        assertEquals(9, y.Max(x));
+    public void test3() {
+        coffe_machine c = new coffe_machine();
+        c.power = true;
+        assertEquals("Error!", c.openMachine());
     }
     @Test
-    public void test4(){
-        MaxMinElements y = new MaxMinElements();
-        int[] x = {1};
-        assertEquals(1,y.Min(x));
-        assertEquals(1, y.Max(x));
+    public void test4() {
+        coffe_machine c = new coffe_machine();
+        c.openMachine();
+        c.closeMachine();
+        assertEquals("Please turn the machine on", c.addMoney(30));
     }
     @Test
-    public void test5(){
-        MaxMinElements y = new MaxMinElements();
-        int[] x = {65, 87, 1, 66, 88, 112, 210, -1};
-        assertEquals(-1,y.Min(x));
-        assertEquals(210, y.Max(x));
+    public void test5() {
+        coffe_machine c = new coffe_machine();
+        c.openMachine();
+        assertEquals("money added Successfully", c.addMoney(30));
+    }
+    @Test
+    public void test6() {
+        coffe_machine c = new coffe_machine();
+        assertEquals("Power on" + "Lights on",c.openMachine());
+        assertEquals("money added Successfully",c.addMoney(30));
+    }
+    @Test
+    public void test7(){
+        coffe_machine c = new coffe_machine();
+        assertEquals("Power on" + "Lights on",c.openMachine());
+        assertEquals("money added Successfully",c.addMoney(30));
+        assertEquals("Lights off" + "Power off",c.closeMachine());
+    }
+    @Test
+    public void test8(){
+        coffe_machine c = new coffe_machine();
+        assertEquals("Please turn the machine on",c.makeCoffee(2));
+    }
+    @Test
+    public void test9(){
+        coffe_machine c = new coffe_machine();
+        assertEquals("Power on" + "Lights on",c.openMachine());
+        assertEquals("money added Successfully",c.addMoney(30));
+        assertEquals(2+ " Coffee successfuly made",c.makeCoffee(3));
+    }
+    @Test
+    public void test10(){
+        coffe_machine c = new coffe_machine();
+        assertEquals("Power on" + "Lights on",c.openMachine());
+        assertEquals("money added Successfully",c.addMoney(30));
+        assertEquals(2+ " Coffee successfuly made",c.makeCoffee(4));
+    }
+    @Test
+    public void test11(){
+        coffe_machine c = new coffe_machine();
+        assertEquals("Power on" + "Lights on",c.openMachine());
+        assertEquals("money added Successfully",c.addMoney(30));
+        assertEquals(1+ " Coffee successfuly made",c.makeCoffee(1));
     }
 }
